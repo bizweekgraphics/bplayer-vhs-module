a385f20a61d3b890a5b1a6cb43ca218c32f0e56b
edweena <jamespants@gmail.com>
2016-09-28 12:45:12 -0400

    handle common.js or browser

--- index.js
+++ index.js
@@ -11,211 +11,233 @@
 // VHS Constructor
 //
 //--------------------------------------------
-		
-var VHS = function(params){
 
-	this.id = params.videoId || null;
-	this.domId = document.getElementById(params.domId) || null;
-	this.autoplay = params.autoplay || false;
-	this.player = null;
-
-	this.options = {};
-	this.callbacks = {};
-	this.src = 'https://cdn.gotraffic.net/projector/latest/bplayer.js';
+(function(){
 
 
-	//--------------------------------------------
-	// Binding
-	//
-	this.initializePlayer = this.initializePlayer.bind(this);
-			
+	var VHS = (function(){
 
-	this.init();
-};
+		var VHS = function(params){
 
+			this.id = params.videoId || null;
+			this.domId = document.getElementById(params.domId) || null;
+			this.autoplay = params.autoplay || false;
+			this.player = null;
 
-
-
-//--------------------------------------------
-//
-// VHS class
-//
-//--------------------------------------------
-		
-VHS.prototype = {
-
-	//--------------------------------------------
-	// Initialization
-	//
-	init: function(){
-
-		var self = this;
-
-		//Make sure required params are set
-		this.checkRequirements();
+			this.options = {};
+			this.callbacks = {};
+			this.src = 'https://cdn.gotraffic.net/projector/latest/bplayer.js';
 
 
+			//--------------------------------------------
+			// Binding
+			//
+			this.initializePlayer = this.initializePlayer.bind(this);
+					
 
-		//Add bplayer defaults + required params
-		this.addOptions();
-
-		//add projector script, with start method as callback
-		this.addScript();
-
-
-		window.addEventListener('BPlayerLoaded', this.initializePlayer);
-
-	},
-
-
-	//--------------------------------------------
-	//
-	// Make sure all required params are in
-	//
-	//--------------------------------------------
-			
-	checkRequirements: function(){
-		//--------------------------------------------
-		// Make sure user has entered ID
-		//
-				
-		if (!this.id){
-			console.warn('🐍 It seems you have not provided a video ID to VHS. Please specify ID.');
-			return null;
-		}
-
-
-		//--------------------------------------------
-		// Make sure there is a DOM node to inject video into
-		//
-		if	(!this.domId){
-			console.warn('👻 It seems you have not provided a DOM node to inject the video into. Please specifiy domId.');
-			return null;
-		}
-
-		//--------------------------------------------
-		// Check to make sure if autoplay is passed in, it is in right type
-		//
-				
-		if (this.autoplay === 'true' || this.autoplay === true){
-			this.autoplay = true;
-		}
-
-		else{
-			this.autoplay = false;
-		}
-	},
-
-
-
-	//--------------------------------------------
-	//
-	// Setup bplayer defaults + user params
-	//
-	//--------------------------------------------
-			
-	addOptions: function(){
-		var self = this;
-
-		this.options = {
-			module_ad_player: 'enabled',
-			use_comscore: true,
-			use_parsely: true,
-			use_mediabong: true,
-			module_conviva_insights: 'enabled',
-			conviva_account: 'c3.Bloomberg',
-			use_share_overlay: true,
-			use_js_ads: true,
-			id: this.id,
-			htmlChildId: 'bbg-video-player-' + this.domId,
-			idType: 'BMMR',
-			serverUrl: 'https://www.bloomberg.com/api/embed',
-			//HTML5 first...
-			techOrder:['html5', 'flash'],
-			log_debug: false,
-			ui_controls_popout: false,
-			ad_tag_gpt_preroll: true,
-			ad_tag_gpt_midroll: true,
-			ad_tag_sz_preroll: '1x7',
-			ad_tag_sz_midroll: '1x7',
-			ad_tag_sz_overlay: '1x7',
-			ad_network_id_preroll: '5262',
-			ad_network_id_midroll: '5262',
-			ad_network_id_overlay: '5262',
-			ad_tag_overlay: 'business/videooverlay',
-			ads_vast_timeout: 10000,
-			ads_playback_timeout: 10000,
-			wmode: 'opaque',
-			comscore_ns_site: 'bloomberg-stream',
-			comscore_page_level_tags: {}, 
-			ads_max_retries_preroll: 3,
-			ads_max_retries_midroll: 3,
-			vertical: 'business',  
-			vertical: 'business', 
-			ad_tag_overlay: 'business/videooverlay', 
-			use_parsely: true, 'source': 'BBIZweb', 
-			module_conviva_insights: 'enabled', 
-			conviva_account: 'c3.Bloomberg', 
-			zone: 'video', 'ad_tag_cust_params_preroll': '',
-			ad_tag: '',
-			ad_tag_midroll: '',
-			offsite_embed: false,    
-			ad_tag_midroll: '',
-			ad_tag_cust_params_preroll: '',
-			autoplay: this.autoplay
+			this.init();
 		};
-	},
 
 
-	//--------------------------------------------
-	// Add script
-	//
-	addScript: function(callback){
-		var self = this;
-		var ready = false;
-		var tag = document.createElement('script');
-		var firstScriptTag = document.getElementsByTagName('script')[0];
-		tag.src = this.src;
-
-		
-		tag.onload = tag.onreadystatechange = function(){
-
-			if (!ready && (!this.readyState || this.readyState === 'complete')){
-				ready = true;
-				self.scriptLoaded = true;
-			}
-		}
-
-		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-	},
 
 
-	//--------------------------------------------
-	//
-	// Initialize player
-	//
-	//--------------------------------------------
+		//--------------------------------------------
+		//
+		// VHS class
+		//
+		//--------------------------------------------
+				
+		VHS.prototype = {
 
-	initializePlayer: function(event){
+			//--------------------------------------------
+			// Initialization
+			//
+			init: function(){
 
-		var self = this;
+				var self = this;
 
-		this.playerVersion = event.detail.version;
-		
-		var bplayer = event.detail.bplayer;
+				//Make sure required params are set
+				this.checkRequirements();
 
-		bplayer.create(this.domId, this.options, {
-			onReady: function(){
-				self.player = this;
+
+
+				//Add bplayer defaults + required params
+				this.addOptions();
+
+				//add projector script, with start method as callback
+				this.addScript();
+
+
+				window.addEventListener('BPlayerLoaded', this.initializePlayer);
+
 			},
 
-			onError: function(err){
-				console.error(err);
+
+			//--------------------------------------------
+			//
+			// Make sure all required params are in
+			//
+			//--------------------------------------------
+					
+			checkRequirements: function(){
+				//--------------------------------------------
+				// Make sure user has entered ID
+				//
+						
+				if (!this.id){
+					console.warn('🐍 It seems you have not provided a video ID to VHS. Please specify ID.');
+					return null;
+				}
+
+
+				//--------------------------------------------
+				// Make sure there is a DOM node to inject video into
+				//
+				if	(!this.domId){
+					console.warn('👻 It seems you have not provided a DOM node to inject the video into. Please specifiy domId.');
+					return null;
+				}
+
+				//--------------------------------------------
+				// Check to make sure if autoplay is passed in, it is in right type
+				//
+						
+				if (this.autoplay === 'true' || this.autoplay === true){
+					this.autoplay = true;
+				}
+
+				else{
+					this.autoplay = false;
+				}
+			},
+
+
+
+			//--------------------------------------------
+			//
+			// Setup bplayer defaults + user params
+			//
+			//--------------------------------------------
+					
+			addOptions: function(){
+				var self = this;
+
+				this.options = {
+					module_ad_player: 'enabled',
+					use_comscore: true,
+					use_parsely: true,
+					use_mediabong: true,
+					module_conviva_insights: 'enabled',
+					conviva_account: 'c3.Bloomberg',
+					use_share_overlay: true,
+					use_js_ads: true,
+					id: this.id,
+					htmlChildId: 'bbg-video-player-' + this.domId,
+					idType: 'BMMR',
+					serverUrl: 'https://www.bloomberg.com/api/embed',
+					//HTML5 first...
+					techOrder:['html5', 'flash'],
+					log_debug: false,
+					ui_controls_popout: false,
+					ad_tag_gpt_preroll: true,
+					ad_tag_gpt_midroll: true,
+					ad_tag_sz_preroll: '1x7',
+					ad_tag_sz_midroll: '1x7',
+					ad_tag_sz_overlay: '1x7',
+					ad_network_id_preroll: '5262',
+					ad_network_id_midroll: '5262',
+					ad_network_id_overlay: '5262',
+					ad_tag_overlay: 'business/videooverlay',
+					ads_vast_timeout: 10000,
+					ads_playback_timeout: 10000,
+					wmode: 'opaque',
+					comscore_ns_site: 'bloomberg-stream',
+					comscore_page_level_tags: {}, 
+					ads_max_retries_preroll: 3,
+					ads_max_retries_midroll: 3,
+					vertical: 'business',  
+					vertical: 'business', 
+					ad_tag_overlay: 'business/videooverlay', 
+					use_parsely: true, 'source': 'BBIZweb', 
+					module_conviva_insights: 'enabled', 
+					conviva_account: 'c3.Bloomberg', 
+					zone: 'video', 'ad_tag_cust_params_preroll': '',
+					ad_tag: '',
+					ad_tag_midroll: '',
+					offsite_embed: false,    
+					ad_tag_midroll: '',
+					ad_tag_cust_params_preroll: '',
+					autoplay: this.autoplay
+				};
+			},
+
+
+			//--------------------------------------------
+			// Add script
+			//
+			addScript: function(callback){
+				var self = this;
+				var ready = false;
+				var tag = document.createElement('script');
+				var firstScriptTag = document.getElementsByTagName('script')[0];
+				tag.src = this.src;
+
+				
+				tag.onload = tag.onreadystatechange = function(){
+
+					if (!ready && (!this.readyState || this.readyState === 'complete')){
+						ready = true;
+						self.scriptLoaded = true;
+					}
+				}
+
+				firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+			},
+
+
+			//--------------------------------------------
+			//
+			// Initialize player
+			//
+			//--------------------------------------------
+
+			initializePlayer: function(event){
+
+				var self = this;
+
+				this.playerVersion = event.detail.version;
+				
+				var bplayer = event.detail.bplayer;
+
+				bplayer.create(this.domId, this.options, {
+					onReady: function(){
+						self.player = this;
+					},
+
+					onError: function(err){
+						console.error(err);
+					}
+				});
 			}
-		});
-	}
+		};
+
+		return VHS;
+
+	})();
+
+
+	//--------------------------------------------
+	// Export to common.js or window, depending
+	//
 			
-}
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
+		module.exports = VHS;
+	}
 
+	else{
+		window.VHS = VHS;
+	}
 
-module.exports = VHS;
+})();
+		
+
